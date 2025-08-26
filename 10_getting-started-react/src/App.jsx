ce735cfbb32a22282c9a3164af55a239c271389a
Francois Dungca <francoisdungca0521@gmail.com>
2025-08-27 03:05:35 +0800

    Activity 10: Add React About Me project

--- 10_getting-started-react/src/App.jsx
+++ 10_getting-started-react/src/App.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 // import reactLogo from './assets/'
 import './App.css'
 
-const title = {
-  // color: "red",
-  // backgroundColor: "black"
-}
-
 const paragraph = {
   
 }
@@ -15,17 +10,23 @@ function App() {
 
   return (
     <>
-      <h1 style={title}>About Me</h1>
+      <div className="section aboutMe">
+        <h1>About Me</h1>
+        <p><strong>Name:</strong> Dungca, Allain Francois D.</p>
+        <p><strong>Course:</strong> Bachelor of Science in Information Systems 3A</p>
+        <p><strong>Fun Fact:</strong> I love creating <b>UI/UX designs</b> and <b>editing videos</b>.</p>
+      </div>
 
-      <p><strong>Name:</strong> Dungca, Allain Francois D.</p>
-      <p><strong>Course:</strong> Bachelor of Science in Information Systems 3A</p>
-      <p><strong>Fun Fact:</strong> I love creating <b>UI/UX designs</b> and <b>editing videos</b>.</p>
-      
-      <h2>Why I Want to Learn React</h2>
-      <p>
-      The reason I want to learn React is to gain a strong understanding of its fundamentals, especially as an aspiring UI/UX designer. It is important for me to know how to work with front-end technologies and understand how they function, because this knowledge allows me to bridge the gap between design and development. In addition, learning React will help me improve and refine my technical skills, making me more versatile as a designer.
-      </p>
-       
+      <div className="section learnReact">
+        <h2>Why I Want to Learn React</h2>
+        <p>
+          The reason I want to learn React is to gain a strong understanding of its fundamentals,
+          especially as an aspiring UI/UX designer. It is important for me to know how to work
+          with front-end technologies and understand how they function, because this knowledge
+          allows me to bridge the gap between design and development. In addition, learning React
+          will help me improve and refine my technical skills, making me more versatile as a designer.
+        </p>
+      </div>
     </>
   )
 }
