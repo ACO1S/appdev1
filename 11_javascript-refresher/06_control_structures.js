d77adf2177885b3062ec21d51348ecb4a186517f
Francois Dungca <francoisdungca0521@gmail.com>
2025-09-06 12:24:49 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,24 @@
+//GRADES
+let score = 65;
+
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else if (score >= 70) {
+  console.log("Grade: C");
+} else {
+  console.log("Grade: F");
+}
+
+//1-5
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//HELLO
+let count = 0;
+while (count < 3) {
+  console.log("Hello");
+  count++;
+}
