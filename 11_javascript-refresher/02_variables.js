0a1eebb18e4f20a8e04dcfaae08e739f2e3b5786
Francois Dungca <francoisdungca0521@gmail.com>
2025-09-06 12:23:36 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,27 @@
+//VARIABLES
+const name = "Francois";
+let age = 20;
+var isStudent = true;
+
+//PRINT
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("IS Student:", isStudent);
+
+//TYPEOF
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+//ARITHMETIC
+let number1 = 21;
+let number2 = 5;
+
+console.log("Addition:", number1 + number2);
+console.log("Subtraction:", number1 - number2);
+console.log("Multiplication:", number1 * number2);
+console.log("Division:", number1 / number2);
+
+//DOUBLE AND TRIPLE EQUALS
+console.log('"5" == 5:', "5" == 5);
+console.log('"5" === 5:', "5" === 5);
