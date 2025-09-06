d2f72c09a7f301c30a60d5ec740048bd4e60dd6a
Francois Dungca <francoisdungca0521@gmail.com>
2025-09-06 14:35:33 +0800

    Add destructuring activity

---
+++ 11_javascript-refresher/12_destructuring.js
@@ -0,0 +1,20 @@
+const person = {
+    name: "Francois",
+    age: 20,
+};
+
+const { name, age } = person;
+console.log("Name:", name);
+console.log("Age:", age);
+
+const hobbies = ["Watching", "Eating", "Sleeping"];
+const [hobby1, hobby2, hobby3] = hobbies;
+console.log("Hobby 1:", hobby1);
+console.log("Hobby 2:", hobby2);
+console.log("Hobby 3:", hobby3);
+
+function printName({ name, age }) {
+    console.log(`Name: ${name}, Age: ${age}`);
+}
+
+printName(person);
