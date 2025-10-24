8e1b987efb3167937de5e844e20ff30c3bfd53c8
Francois Dungca <francoisdungca0521@gmail.com>
2025-10-24 23:27:46 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,24 +1,6 @@
 import './App.css'
+import Gallery from './Gallery.jsx';
 
-function Profile() {
-  return (
-    <img
-      src="https://i.scdn.co/image/ab67616d00001e02394048503e3be0e65e962638"
-      alt="Multo"
-    />
-  );
-}
-
-function Gallery() {
-  return (
-    <section>
-      <h1>Song of the Year</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
-}
 
 export default function App() {
   return (
---
+++ react-describing-ui/src/Gallery.jsx
@@ -0,0 +1,12 @@
+import Profile from './Profile.jsx';
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Song of the Year</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
