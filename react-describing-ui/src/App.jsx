f7e15d6691f35d2b034056c5f5cea94bd27eefbd
Francois Dungca <francoisdungca0521@gmail.com>
2025-10-24 23:34:41 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
-import Gallery from './Gallery.jsx';
 
-
-export default function App() {
+export default function TodoList() {
   return (
     <>
-      <Gallery />
+      <h1>2025 HIT SONG</h1>
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02394048503e3be0e65e962638"
+        alt="Multo"
+        className="photo"
+      />
+      <ul>
+        <li>MULTO</li>
+        <li>Cup of Joe</li>
+        <li>Song of the Year</li>
+      </ul>
     </>
   );
 }
+
--- react-describing-ui/src/Profile.jsx
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <img
-      src="https://i.scdn.co/image/ab67616d00001e02394048503e3be0e65e962638"
-      alt="Multo"
-    />
-  );
-}
