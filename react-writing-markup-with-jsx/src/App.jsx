42c925a5e800776ca0cb52eacd85c14b728631c1
Francois Dungca <francoisdungca0521@gmail.com>
2025-11-09 20:16:23 +0800

    Converting HTML to JSX

---
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function TodoList() {
+  return (
+    <h1>Hedy Lamarr's Todos</h1>
+    <img 
+      src="https://i.imgur.com/yXOvdOSs.jpg" 
+      alt="Hedy Lamarr" 
+      class="photo"
+    >
+    <ul>
+      <li>Invent new traffic lights
+      <li>Rehearse a movie scene
+      <li>Improve the spectrum technology
+    </ul>
+  );
+}
